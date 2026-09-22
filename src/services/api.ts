2cbd45774b0a630c852ea07d9663a60a8f8076b9
Alfaro dwi sakti <alfarodwisakkti@gmail.com>
2026-09-22 16:47:13 +0700

    y

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { Siswa, PresensiRecord, UserSession, ApiResponse, RekapHarianData, RekapPeriodeData, StatusPresensi, SiswaRekapStat } from '../types';
 
 export const DEFAULT_KELAS = "8.G";
-export const JAM_BATAS_TERLAMBAT = "07:15";
+export const JAM_BATAS_TERLAMBAT = "23:59";
 const DEFAULT_API_URL = "https://script.google.com/macros/s/AKfycbwx6Zu1e3ZoZI6plItKreD8qeWscV1g8YnKGvgo8Rg0ZWDBtD4M3_yAgSX-JUj0jDjS7w/exec";
 
 const INITIAL_SISWA: Siswa[] = [];
